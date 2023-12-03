ac582430e8dda05af8c818d95875e1c7f64c8c19
Zibril Yuan <zyuuannn@gmail.com>
2023-12-03 17:06:32 +0700

    Add v1.1

---
+++ Editor/Packages.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Tsukuyomi.Tools
+{
+    public static class Packages
+    {
+        #region Tsukuyomi Callbacks
+
+        public static void InstallUnityPackage(string packageName)
+        {
+            if (IsPackageInstalled(packageName))
+            {
+                Debug.LogWarning($"com.unity.{packageName} already installed!");
+                return;
+            }
+            
+            UnityEditor.PackageManager.Client.Add($"com.unity.{packageName}");
+        }
+        
+        private static bool IsPackageInstalled(string packageId)
+        {
+            if ( !File.Exists("Packages/manifest.json") )
+                return false;
+ 
+            string jsonText = File.ReadAllText("Packages/manifest.json");
+            return jsonText.Contains(packageId);
+        }
+
+        #endregion
+    }
+}
--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -1,151 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace Tsukuyomi.Tools
 {
     public class ToolsMenu : EditorWindow
     {
-        #region Variable
-
-        private static string projectName = "Project";
-        private static string[] currentSubFolder;
-
-        // Folder Name
-        private static string[] mainFolders = 
-        {
-            "Art",
-            "Audio",
-            "Data",
-            "Prefabs",
-            "Property",
-            "Scenes",
-            "Scripts",
-            "ScriptableObject"
-        };
-        
-        private static string[] artSubFolders =
-        {
-            "Animation",
-            "Sprites",
-            "Tilemap",
-            "Timeline",
-            "UI"
-        };
-        
-        private static string[] audioSubFolders =
-        {
-            "Music",
-            "SoundEffect"
-        };
-        
-        private static string[] propertySubFolders =
-        {
-            "Presets",
-            "Fonts",
-            "Materials",
-            "PhysicMaterials",
-            "TileRule"
-        };
-        
-        private static string[] sceneSubFolders =
-        {
-            "Main",
-            "Feature",
-            "Asset"
-        };
-        
-        private static string[] scriptSubFolders =
-        {
-            "DesignPattern",
-            "Entities",
-            "Gameplay",
-            "Managers",
-            "Puzzle",
-            "Quest",
-            "ScriptableObject"
-        };
-
-        #endregion
-
-        #region EditorWindow Callbacks
-
-        private void OnGUI()
-        {
-            EditorGUILayout.LabelField("Insert the Project name used as the root folder");
-            projectName = EditorGUILayout.TextField("Project Name: ", projectName);
-            this.Repaint();
-            GUILayout.Space(70);
-
-            if (GUILayout.Button("Generate"))
-            {
-                CreateDefaultFolders();
-                this.Close();
-            }
-        }
-
-        #endregion
-        
-        #region Tsukuyomi Callbacks
-
-        [MenuItem("Tools/Setup/Create Default Folders")]
+        [MenuItem("Tools/Setup/Default Folders")]
         private static void SetUpFolders()
         {
-            ToolsMenu window = CreateInstance<ToolsMenu>();
+            Folders window = CreateInstance<Folders>();
             window.position = new Rect(Screen.width / 2, Screen.height / 2, 400, 150);
             window.ShowPopup();
         }
         
-        private static void CreateDefaultFolders()
-        {
-            var i = 0;
-            string[] roots = 
-            {
-                projectName,
-                projectName + "/Art",
-                projectName + "/Audio",
-                projectName + "/Property",
-                projectName + "/Scenes",
-                projectName + "/Scripts"
-            };
-            
-            foreach (var root in roots)
-            {
-                SubFolderSwitcher(i);
-                FolderDirector("_" + root, currentSubFolder);
-                i++;
-            }
-            
-            AssetDatabase.Refresh();
-        }
+        [MenuItem("Tools/Setup/Packages/Profile Analyzer")]
+        private static void AddProfileAnalyzer() => Packages.InstallUnityPackage("performance.profile-analyzer");
         
-        private static void FolderDirector(string rootName, params string[] dirName)
-        {
-            var fullPath = Path.Combine(Application.dataPath, rootName);
-            foreach (var folder in dirName)
-            {
-                if (!Directory.Exists(Path.Combine(rootName, folder)))
-                    Directory.CreateDirectory(Path.Combine(fullPath, folder));
-            }
-        }
+        [MenuItem("Tools/Setup/Packages/Memory Profiler")]
+        private static void AddMemoryProfiler() => Packages.InstallUnityPackage("memoryprofiler");
 
-        private static void SubFolderSwitcher(int index)
-        {
-            currentSubFolder = index switch
-            {
-                0 => mainFolders,
-                1 => artSubFolders,
-                2 => audioSubFolders,
-                3 => propertySubFolders,
-                4 => sceneSubFolders,
-                5 => scriptSubFolders,
-                _ => throw new ArgumentOutOfRangeException(nameof(index), index, null)
-            };
-        }
-
-        #endregion
+        [MenuItem("Tools/Setup/Packages/New Input System")]
+        private static void AddNewInputSystem() => Packages.InstallUnityPackage("inputsystem");
         
+        [MenuItem("Tools/Setup/Packages/Cinemachine")]
+        private static void AddCinemachine() => Packages.InstallUnityPackage("cinemachine");
+
     }
 }
